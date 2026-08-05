a1ffcfc9f5df704f882a4227419a2ea5be0ebc54
Ashley Shang <ashley@acube.com.tw>
2026-08-05 10:41:14 +0800

    fix: adjust loading and layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+export default function AuthLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="h-[100vh] flex items-center justify-center p-4 bg-muted/30">
+            <div className="w-full max-w-sm">
+                <div className='text-primary text-5xl font-bold text-center mb-5 font-[family-name:var(--font-bevan)]'>Tabuddy</div>
+                {children}</div>
+        </div>
+    )
+}
